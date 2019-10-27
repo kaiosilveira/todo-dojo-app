14e4c9bdc6215e68bd20449c07f2c53bba12e6dd
Kaio Silveira <silveira.kaio@outlook.com>
2019-10-27 10:37:35 -0300

    Dojo 26/10/2019

--- src/App.js
+++ src/App.js
@@ -1,26 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
-function App() {
+const DateBox = () => {
+  const now = new Date()
+  const months = [
+    'jan',
+    'fev',
+    'mar',
+    'abr',
+    'mai',
+    'jun',
+    'jul',
+    'ago',
+    'set',
+    'out',
+    'nov',
+    'dez'
+  ]
+  const weekdays = [
+    'Domingo',
+    'Segunda-feira',
+    'Terca-feira',
+    'Quarta-feira',
+    'Quinta-feira',
+    'Sexta-feira',
+    'Sabado'
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='date-box'>
+      <div className='date-info'>
+        <p className='day'>{now.getDate()}</p>
+        <div className='month-year'>
+          <p className='month-name'>{months[now.getMonth()]}</p>
+          <p>{now.getFullYear()}</p>
+        </div>
+      </div>
+      <div className='weekday-name'>
+        <p>{weekdays[now.getDay()]}</p>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+const TodoList = ({ toDos }) => {
+  return (
+    <ul className='todo-list'>
+      {toDos.map(item => {
+        return (
+          <li className={'todo-list-item' + (item.done ? ' done' : '')}>
+            {item.text}
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+const AddItem = ({ onSave }) => {
+  const [text, setText] = useState('')
+  return (
+    <div className='add-item'>
+      <input
+        className='add-field'
+        value={text}
+        placeholder='Adicionar um item...'
+        onChange={e => {
+          setText(e.target.value)
+        }}
+      />
+      <button
+        className='add-button'
+        onClick={() => {
+          onSave({ text: text, done: false })
+          setText('')
+        }}
+      >
+        +
+      </button>
+    </div>
+  )
+}
+const App = () => {
+  const [toDos, setTodos] = useState([
+    { text: 'texte1', done: true },
+    { text: 'texte2', done: false },
+    { text: 'texte3', done: false }
+  ])
+
+  return (
+    <div className='App'>
+      <DateBox />
+      <TodoList toDos={toDos} />
+      <AddItem onSave={item => setTodos([...toDos, item])} />
+    </div>
+  )
+}
+
+export default App
